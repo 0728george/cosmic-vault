a4ff0c0eba66cf0d92d84f1284452fe9fc17783f
Gheorghe Varga <149835163+0728george@users.noreply.github.com>
2025-11-19 16:39:03 -0500

    feat: add Document TypeScript type

---
+++ src/types/documents.ts
@@ -0,0 +1,11 @@
+export type Document = {
+  slug: string;
+  title: string;
+  author: string;
+  year: string;
+  license: string;
+  tags: string[];
+  description: string;
+  source: string;
+  file: string;
+};
