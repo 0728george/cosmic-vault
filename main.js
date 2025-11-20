bdebf9bba36b34ea8ad4472518df1707b81a6297
Gheorghe Varga <149835163+0728george@users.noreply.github.com>
2025-11-20 16:40:10 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import './index.css'  // Tailwind CSS
+
+const app = createApp(App)
+app.use(createPinia())
+app.mount('#app')
